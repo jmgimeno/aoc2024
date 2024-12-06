afd0f9a621cf945d2bcc259b9fa663aed5b5895c
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2024-12-06 09:44:35 +0100

    Solved day 6 (in Scala) with futures in part2

--- days/src/main/scala/aoc2024s/day6/Day6.scala
+++ days/src/main/scala/aoc2024s/day6/Day6.scala
@@ -3,7 +3,8 @@ package aoc2024s.day6;
 
 import utils.IO
 
-import java.util.concurrent.atomic.AtomicLong
+import scala.concurrent.*
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.jdk.CollectionConverters.*
 
 case class Position(x: Int, y: Int)
@@ -71,6 +72,7 @@ object Day6 {
 
     case class State(guard: Guard, steps: Set[Guard], obstacle: Option[Position]) {
       def step: State = copy(guard = guard.step, steps = steps + guard)
+
       def turnRight: State = copy(guard = guard.turnRight, steps = steps + guard)
     }
 
@@ -117,19 +119,12 @@ object Day6 {
     val numThreads = Runtime.getRuntime.availableProcessors
     val chunkSize = steps.size / numThreads
     val chunks = steps.grouped(chunkSize).toList
-    val result: AtomicLong = new AtomicLong(0L)
-    val threads = chunks.map { chunk =>
-      val thread = new Thread {
-        override def run(): Unit = {
-          val counter = sequentialCount(plan, guard, chunk)
-          result.accumulateAndGet(counter, _ + _)
-        }
+    val futures = chunks.map { chunk =>
+      Future {
+        sequentialCount(plan, guard, chunk)
       }
-      thread.start()
-      thread
     }
-    threads.foreach(_.join())
-    result.get
+    futures.map(future => Await.result(future, duration.Duration.Inf)).sum
   }
 
   @main def main6(): Unit = {
