3ed0bc4e3d638920419848653962e7d5bd5b6c8c
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2024-12-11 07:55:54 +0100

    Minor simplifications

--- days/src/main/scala/aoc2024s/day11/Day11.scala
+++ days/src/main/scala/aoc2024s/day11/Day11.scala
@@ -11,13 +11,11 @@ object Day11 {
   case class Stones(stones: Map[Long, Long]) {
 
     def this(data: String) =
-      this(data
-        .split(" ")
-        .map(_.toLong)
-        .groupBy(identity)
-        .map { case (value, values) =>
-          value -> values.length.toLong
-        })
+      this(
+        data
+          .split(" ")
+          .groupMapReduce(_.toLong)(_ => 1L)(_ + _)
+      )
 
     def numStones: Long = stones.values.sum
 
@@ -27,35 +25,36 @@ object Day11 {
       }
     }
 
-    def oneBlink: Stones = {
+    private def oneBlink: Stones = {
       val newCounters =
-        stones.toSeq.flatMap { case (value, counter) =>
-          blinkValue(value).map {
-            _ -> counter
+        stones.map { (value, counter) =>
+            blinkValue(value).map {
+              _ -> counter
+            }
           }
-        }.groupBy(_._1).map { case (value, pairs) =>
-          value -> pairs.map(_._2).sum
-        }
+          .flatten // flatMap != map + flatten !!!
+          .groupMapReduce(_._1)(_._2)(_ + _)
 
       Stones(newCounters)
     }
 
-    def numberOfDigits(value: Long): Int = {
+    private def numberOfDigits(value: Long): Int = {
       @tailrec
       def go(value: Long, acc: Int): Int =
         if value < 10 then acc
         else go(value / 10, acc + 1)
+
       go(value, 1)
     }
 
-    def pow10(exp: Int): Long = {
+    private def pow10(exp: Int): Long = {
       (1 to exp)
         .foldLeft(1L) { case (acc, _) =>
           acc * 10
         }
     }
 
-    def blinkValue(value: Long): Seq[Long] =
+    private def blinkValue(value: Long): Seq[Long] =
       if value == 0 then Seq(1)
       else
         val n = numberOfDigits(value)
