4e1d0d1bb378756c803f8cb4576f2f33554e4ca3
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2024-12-17 13:13:59 +0100

    Wip on day 17 part 2

--- days/src/main/scala/aoc2024s/day17/Day17.scala
+++ days/src/main/scala/aoc2024s/day17/Day17.scala
@@ -7,13 +7,8 @@ import scala.jdk.CollectionConverters.*
 
 object Day17 {
 
-  type Register = Int
-  type Output = IndexedSeq[Int]
-  type Program = List[Int]
-
   enum OpCode {
     case ADV, BXL, BST, JNZ, BXC, OUT, BDV, CDV
-
   }
 
   import OpCode.*
@@ -21,7 +16,7 @@ object Day17 {
   object Disassembler {
 
     private def combo(arg: Int): String = arg match {
-      case 0 | 1 | 2 | 3  => literal(arg)
+      case 0 | 1 | 2 | 3 => literal(arg)
       case 4 => "@a"
       case 5 => "@b"
       case 6 => "@c"
@@ -30,7 +25,7 @@ object Day17 {
 
     private def literal(arg: Int): String = s"#$arg"
 
-    def assembled(opCode: OpCode, arg: Int): String =
+    private def assembled(opCode: OpCode, arg: Int): String =
       opCode match {
         case ADV => s"ADV ${combo(arg)}"
         case BXL => s"BXL ${literal(arg)}"
@@ -42,18 +37,18 @@ object Day17 {
         case CDV => s"CDV ${combo(arg)}"
       }
 
-    def disassemble(program: Program): String =
+    def disassemble(program: List[Int]): String =
       program.zipWithIndex.grouped(2).toList.map {
         case List((op, i), (arg, _)) => f"$i%2d : ${assembled(OpCode.fromOrdinal(op), arg)}"
-        case List((op, i)) => f"$i%2d: assembled(OpCode.fromOrdinal(op), 0)}"
+        case _ => sys.error("invalid program")
       }.mkString("\n")
   }
 
-  case class Computer(a: Register, b: Register, c: Register, ip: Int, output: Output) {
+  case class Computer(a: Int, b: Int, c: Int, ip: Int, output: List[Int]) {
 
     assert(a >= 0 && b >= 0 && c >= 0)
 
-    def combo(operand: Int): Int = operand match {
+    private def combo(operand: Int): Int = operand match {
       case 0 | 1 | 2 | 3 => operand
       case 4 => a
       case 5 => b
@@ -87,28 +82,43 @@ object Day17 {
 
   object Executor {
 
-    def run(computer: Computer, program: Program): Computer = {
+    def run(computer: Computer, program: List[Int]): Computer = {
       @tailrec def go(
         computer: Computer,
-        program: Program
       ): Computer = {
         if computer.ip >= program.length then computer
         else {
           val opcode = OpCode.fromOrdinal(program(computer.ip))
           val operand = program(computer.ip + 1)
           val computer1 = computer.execute(opcode, operand)
-          go(computer1, program)
+          go(computer1)
         }
       }
-      go(computer, program)
+
+      go(computer)
     }
   }
 
-  def parse(data: List[String]): (Computer, Program) = {
-    val registers = data.take(3).map(_.split(" ")(2).toInt)
+  private class FixPointFinder(computer: Computer, output: List[Int]) {
+
+    private def nextMachine(n: Int): Computer = computer.copy(a = n)
+
+    private def isFixedPoint(n: Int): Boolean = ??? // TODO
+
+    def fixPoint: Int =
+      @tailrec
+      def go(n: Int): Int =
+        if isFixedPoint(n) then n
+        else go(n + 1)
+
+      go(0)
+  }
+
+  def parse(data: List[String]): (Computer, List[Int]) = {
+    val List(a, b, c) = data.take(3).map(_.split(" ")(2).toInt)
     val program = data.last.split(": ")(1).split(",").map(_.toInt).toList
     (
-      Computer(registers(0), registers(1), registers(2), 0, IndexedSeq.empty),
+      Computer(a, b, c, 0, List.empty),
       program
     )
   }
@@ -118,8 +128,9 @@ object Day17 {
     Executor.run(computer, program).output.mkString(",")
   }
 
-  def part2(data: List[String]): Long = {
-    throw new UnsupportedOperationException("part2")
+  def part2(data: List[String]): Int = {
+    val (computer, program) = parse(data)
+    FixPointFinder(computer, program).fixPoint
   }
 
   @main def main17(): Unit = {
--- days/src/test/scala/aoc2024s/day17/Day17Suite.scala
+++ days/src/test/scala/aoc2024s/day17/Day17Suite.scala
@@ -9,15 +9,23 @@ import munit.FunSuite
 
 class Day17Suite extends FunSuite {
 
-  val example: String =
+  val part1: String =
     """Register A: 729
       |Register B: 0
       |Register C: 0
       |
       |Program: 0,1,5,4,3,0""".stripMargin
 
+  val part2: String =
+    """Register A: 2024
+      |Register B: 0
+      |Register C: 0
+      |
+      |Program: 0,3,5,4,3,0
+      |""".stripMargin
+
   test("part1 - example data") {
-    val data = IO.splitLinesAsList(example).asScala.toList
+    val data = IO.splitLinesAsList(part1).asScala.toList
     assertEquals(Day17.part1(data), "4,6,3,5,6,3,5,2,1,0")
   }
 
@@ -27,48 +35,56 @@ class Day17Suite extends FunSuite {
   }
 
   test("part2 - example data") {
-    val data = IO.splitLinesAsList(example).asScala.toList
-    assertEquals(Day17.part2(data), -1L)
+    val data = IO.splitLinesAsList(part2).asScala.toList
+    assertEquals(Day17.part2(data), 117440)
   }
 
   test("part2 - input data") {
     val data = IO.getResourceAsList("aoc2024/day17.txt").asScala.toList
-    assertEquals(Day17.part2(data), -1L)
+    assertEquals(Day17.part2(data), -1)
   }
 
   test("If register C contains 9, the program 2,6 would set register B to 1.") {
-    val machine = Computer(0, 0, 9, 0, IndexedSeq.empty)
+    val machine = Computer(0, 0, 9, 0, List.empty)
     val program = List(2, 6)
     val result = Executor.run(machine, program)
     assertEquals(result.b, 1)
   }
 
   test("If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.") {
-    val machine = Computer(10, 0, 0, 0, IndexedSeq.empty)
+    val machine = Computer(10, 0, 0, 0, List.empty)
     val program = List(5, 0, 5, 1, 5, 4)
     val result = Executor.run(machine, program)
-    assertEquals(result.output, IndexedSeq(0, 1, 2))
+    assertEquals(result.output, List(0, 1, 2))
   }
 
   test("If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.") {
-    val machine = Computer(2024, 0, 0, 0, IndexedSeq.empty)
+    val machine = Computer(2024, 0, 0, 0, List.empty)
     val program = List(0, 1, 5, 4, 3, 0)
     val result = Executor.run(machine, program)
-    assertEquals(result.output, IndexedSeq(4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0))
+    assertEquals(result.output, List(4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0))
     assertEquals(result.a, 0)
   }
 
   test("If register B contains 29, the program 1,7 would set register B to 26.") {
-    val machine = Computer(0, 29, 0, 0, IndexedSeq.empty)
+    val machine = Computer(0, 29, 0, 0, List.empty)
     val program = List(1, 7)
     val result = Executor.run(machine, program)
     assertEquals(result.b, 26)
   }
 
   test("If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.") {
-    val machine = Computer(0, 2024, 43690, 0, IndexedSeq.empty)
+    val machine = Computer(0, 2024, 43690, 0, List.empty)
     val program = List(4, 0)
     val result = Executor.run(machine, program)
     assertEquals(result.b, 44354)
+  }
+
+  test("part 1 - on example2") {
+    val data = IO.splitLinesAsList(part2).asScala.toList
+    val (machine, program) = Day17.parse(data)
+    val modified = machine.copy(a = 117440)
+    val result = Executor.run(modified, program)
+    assertEquals(result.output, program)
   }
 }
